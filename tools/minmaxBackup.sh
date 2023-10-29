81a249804d44bf8bb486c49e07bcea01929f1fc2
Peter L <plix1014@gmail.com>
2023-10-29 15:53:57 +0100

    use gcal on raspi

--- tools/minmaxBackup.sh
+++ tools/minmaxBackup.sh
@@ -18,11 +18,24 @@
 WOSPI_HOME=/home/wospi/wetter
 BK_DIR=/home/wospi/backup
 
+DEBUG=1
+
 YY=$(date +'%Y')
 MM=$(date +'%m')
 DD=$(date +'%d')
 
-CAL=cal
+# try to find cal program
+CAL=$(which cal)
+if [ -z "$CAL" ]; then
+    CAL=$(which ncal)
+    if [ -z "$CAL" ]; then
+	CAL=$(which gcal)
+    fi
+fi
+if [ -z "$CAL" ]; then
+    echo "ERROR: could not find 'cal' program. Exiting."
+    exit 2
+fi
 
 OS=$(uname -s)
 ARCH=$(uname -m)
@@ -34,7 +47,8 @@ else
 	OPT="-h"
     elif [ "$ARCH" = "armv7l" ]; then
 	OPT="-bh"
-	CAL=ncal
+    elif [ "$ARCH" = "aarch64" ]; then
+	OPT="-H no"
     else
 	OPT=
     fi
@@ -62,16 +76,21 @@ TMP=$(grep "^SCPTARGET" $WOSPI_HOME/config.py \
 
 LASTDAY=$($CAL $OPT $MMYY | tail -n +3 | tr '\n' ' ' | sort -n | awk 'END {print $NF}')
 
-echo "#------- DEBUG -------"
-echo "CAL    : $CAL $OPT $MMYY | tail -n +3 | tr '\n' ' ' | sort -n | awk 'END {print \$NF}'"
-echo "YY     : $YY"
-echo "MM     : $MM"
-echo "DD     : $DD"
-echo "LASTDAY: $LASTDAY"
-echo "SOURCE : $TMPDIR$MINMAXFILE"
-echo "TARGET : $BK_DIR/$MINMAXFILE.${YY}${MM}${DD}"
-echo "CHECK  : [ $LASTDAY -eq $DD ]"
-echo "#------- DEBUG -------"
+if [ $DEBUG ]; then
+    echo "#------- DEBUG -------"
+    echo "CAL    : $CAL $OPT $MMYY | tail -n +3 | tr '\n' ' ' | sort -n | awk 'END {print \$NF}'"
+    echo "YY     : $YY"
+    echo "MM     : $MM"
+    echo "DD     : $DD"
+    echo "LASTDAY: $LASTDAY"
+    echo "SOURCE : $TMPDIR$MINMAXFILE"
+    echo "TARGET : $BK_DIR/$MINMAXFILE.${YY}${MM}${DD}"
+    echo "CHECK  : [ $LASTDAY -eq $DD ]"
+    echo "#------- DEBUG -------"
+fi
+
+[ ! -d "$BK_DIR" ] && mkdir -p $BK_DIR
+
 
 if [ $LASTDAY -eq $DD ]; then
     echo "$YY-$MM-$DD: today is last day of month. Backing up min-max values."
@@ -81,10 +100,15 @@ else
     echo "$YY-$MM-$DD: today is not the last day of month. Nothing to do."
 fi
 
-echo "$YY-$MM-$DD: backup png files"
-echo "----------------------"
 cd $TMP
-tar cvf $BK_DIR/$YY$MM.png_backup.tar *.png
-echo "----------------------"
-echo "$YY-$MM-$DD: backup done"
+if [ $(ls *.png > /dev/null 2>&1) ]; then
+    echo "$YY-$MM-$DD: backup png files"
+    echo "----------------------"
+    tar cvf $BK_DIR/$YY$MM.png_backup.tar *.png
+    echo "----------------------"
+    echo "$YY-$MM-$DD: backup done"
+else
+    echo "$YY-$MM-$DD: no png files found"
+
+fi
 
